6e2b3721f1dc70123e88657ea412be62ea02eade
Krisztian Hubner <krisztianhubner@gmail.com>
2025-09-29 18:11:09 +0200

    Refactor zsh configuration: separate aliases and keybindings into dedicated files

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -1,2 +1,7 @@
+# Docker
 alias opendock="open --background -a Docker"
 alias quitdock="pkill -f Docker"
+
+# Python
+alias python=python3
+alias pip=pip3
---
+++ oh-my-zsh-custom/bindings.zsh
@@ -0,0 +1,2 @@
+# Keybindings 
+bindkey '^U' backward-kill-line
